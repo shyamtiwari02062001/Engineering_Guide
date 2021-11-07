617c8574414764e739f3183c90982ecfbbf2799c
shyamtiwari02062001 <shyam02062001@gmail.com>
2021-11-07 11:17:13 +0530

    data fetched succesfully from firebase

--- lib/screens/syllabus.dart
+++ lib/screens/syllabus.dart
@@ -1,3 +1,4 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:engineering_guide/widgets/drawer.dart';
 import 'package:flutter/material.dart';
 
@@ -12,6 +13,34 @@ class Syllabus extends StatelessWidget {
         title: const Text("Syllabus"),
       ),
       drawer: const DrawerWidget(),
+      body: StreamBuilder(
+        stream: FirebaseFirestore.instance.collection("syllabus").snapshots(),
+        builder: (BuildContext context, AsyncSnapshot<dynamic> snapshot) {
+          if (snapshot.hasData) {
+            final data = snapshot.data.docs;
+            return ListView.builder(
+              itemCount: data.length,
+              itemBuilder: (ctx, index) {
+                final value = data[index].data();
+                if (index % 2 == 0) {
+                  return ListTile(
+                    leading: Text(value['sem']),
+                    title: Text(value['courseName']),
+                    trailing: Text(value["subject"]),
+                  );
+                } else {
+                  return ListTile(
+                    leading: Text(value['sem']),
+                    title: Text(value['courseName']),
+                    trailing: Text(value["subject"]),
+                  );
+                }
+              },
+            );
+          }
+          return const CircularProgressIndicator();
+        },
+      ),
     );
   }
 }
